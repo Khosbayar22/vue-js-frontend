e79e53c04e7325cfe568e5aca997940711c5242b
Khosbayar Ariunbat <74040410+Two-2-Two@users.noreply.github.com>
2021-10-10 20:46:33 +0800

    first commit

---
+++ vue-js-frontend/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="section">
+        <h2 class="title-text">Friend List</h2>
+        <ul>
+            <li>
+                <friend-contact></friend-contact>
+                <friend-contact></friend-contact>
+            </li>
+        </ul>
+    </div>
+</template>
+<script>
+    export default {
+        data() {
+            return {}
+        }
+    }
+</script>
+
+<style>
+.title-text {
+    color: #fff;
+}
+ul {
+    list-style: none;
+}
+</style>
---
+++ vue-js-frontend/src/components/FriendContact.vue
@@ -0,0 +1,53 @@
+<template>
+    <section class="card-item">
+        <div>
+            <h2>{{friendList.name}}</h2>
+            <ul v-if="detailsAreVisible">
+                <li>{{friendList.id}}</li>
+                <li>{{friendList.age}}</li>
+                <li>{{friendList.phone}}</li>
+            </ul>
+            <button @click="toggleDetails">Detail</button>
+        </div>
+    </section>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                friendList: {
+                    id: 1, 
+                    name: "Bataa", 
+                    age: 19,
+                    phone: "8000-0000",
+                }, 
+                detailsAreVisible: false,
+            }
+        }, 
+        methods: {
+            toggleDetails() {
+                this.detailsAreVisible = !this.detailsAreVisible;
+            }
+        }
+    } 
+</script>
+
+<style>
+body {
+    background: #333;
+}
+.card-item {
+    background: #fff;
+    width: 300px;
+    margin: 8px;
+    border-radius: 8px;
+    padding: 16px;
+}
+h2 {
+    margin: 0;
+    color: #000 ;
+}
+ul {
+    margin: 8px 0;
+}
+</style>
